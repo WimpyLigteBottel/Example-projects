7fcd4e4fbdb1d2f9be8a392423d53991834b6223
Marco-PC\Marco <epossmurfmarco@gmail.com>
2022-09-21 17:45:50 +0200

    adding design project (commiting in junk ;_;)

---
+++ example-design-jpa-project/src/main/kotlin/com/example/marcodemodesign/MarcoDemoDesignApplication.kt
@@ -0,0 +1,11 @@
+package com.example.marcodemodesign
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarcoDemoDesignApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarcoDemoDesignApplication>(*args)
+}
---
+++ example-design-jpa-project/src/test/kotlin/com/example/marcodemodesign/MarcoDemoDesignApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.marcodemodesign
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarcoDemoDesignApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
