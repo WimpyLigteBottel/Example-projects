db88563034d792dfae51d7ba43ccb696330efe3b
Marco-PC\Marco <epossmurfmarco@gmail.com>
2023-06-24 23:03:59 +0200

    Changing my url shortner to make use of random port

--- UrlShortner/src/main/java/nel/marco/Launcher.java
+++ UrlShortner/src/main/java/nel/marco/Launcher.java
@@ -4,10 +4,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import nel.marco.util.ServiceDiscoveryStartupUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -29,114 +32,121 @@ import java.util.concurrent.TimeUnit;
 @SpringBootApplication
 public class Launcher {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Launcher.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
 }
 
 @Component // This is used for another project service discovery
 class StartupSetup implements CommandLineRunner {
 
-  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  @Value("${server.port}")
-  String serverPort;
+    @Autowired
+    private WebServerApplicationContext applicationContext;
 
-  @Value("${application.name}")
-  String applicationName;
+    @Value("${application.name}")
+    String applicationName;
 
-  @Value("${discovery.service.port}")
-  String discoveryServicePort;
+    @Value("${discovery.service.port}")
+    String discoveryServicePort;
 
-  @Override
-  public void run(String... args) throws Exception {
-    ServiceDiscoveryStartupUtil.registerService(serverPort, discoveryServicePort, applicationName);
-  }
+    @Override
+    public void run(String... args) throws Exception {
+
+        String port = applicationContext.getWebServer().getPort() + "";
+        ServiceDiscoveryStartupUtil.registerService(port, discoveryServicePort, applicationName);
+    }
 }
 
 @RestController
 @EnableScheduling
 class RestEndpoint {
 
-  @Value("${ownserverurl}")
-  private String ownServerUrl;
+    @Value("${ownserverurl}")
+    private String ownServerUrl;
 
-  private final Logger logger = LoggerFactory.getLogger(this.getClass());
-  private final ExecutorService executor = Executors.newFixedThreadPool(1);
+    @Autowired
+    private WebServerApplicationContext applicationContext;
 
-  private final Map<String, Map.Entry<String, LocalDateTime>> map = new HashMap<>(10000);
 
-  @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
-  public void cleanup() {
-    executor.submit(
-        () -> {
-          var stringEntryMap = Map.copyOf(map);
-          LocalDateTime now = LocalDateTime.now();
-          stringEntryMap.forEach(
-              (s, pair) -> {
-                if (now.isAfter(pair.getValue())) {
-                  map.remove(s);
-                }
-              });
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final ExecutorService executor = Executors.newFixedThreadPool(1);
 
-          MessageFormat messageFormat = new MessageFormat("Map has been cleaned [currentsize={0}]");
-          String text = messageFormat.format(new Object[] {map.size()});
-          logger.info(text);
-        });
-  }
+    private final Map<String, Map.Entry<String, LocalDateTime>> map = new HashMap<>(10000);
 
-  @GetMapping("/fillMap")
-  public ResponseEntity<String> fillInternalMap() {
+    @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
+    public void cleanup() {
+        executor.submit(
+                () -> {
+                    var stringEntryMap = Map.copyOf(map);
+                    LocalDateTime now = LocalDateTime.now();
+                    stringEntryMap.forEach(
+                            (s, pair) -> {
+                                if (now.isAfter(pair.getValue())) {
+                                    map.remove(s);
+                                }
+                            });
 
-    for (int i = 0; i < 1000000; i++) {
-      String encodedText = generateUniquePattern();
-
-      map.put(encodedText, Map.entry(encodedText, LocalDateTime.now().plusSeconds(5)));
+                    MessageFormat messageFormat = new MessageFormat("Map has been cleaned [currentsize={0}]");
+                    String text = messageFormat.format(new Object[]{map.size()});
+                    logger.info(text);
+                });
     }
 
-    MessageFormat messageFormat = new MessageFormat("Map has been filled [size={0}]");
-    String text = messageFormat.format(new Object[] {map.size()});
-    return ResponseEntity.ok(text);
-  }
+    @GetMapping("/fillMap")
+    public ResponseEntity<String> fillInternalMap() {
 
-  @GetMapping("/generate")
-  public ResponseEntity<String> generate(@RequestParam String text) {
-    String encodedText = generateUniquePattern();
+        for (int i = 0; i < 1000000; i++) {
+            String encodedText = generateUniquePattern();
 
-    map.put(encodedText, Map.entry(text, LocalDateTime.now().plusSeconds(60)));
+            map.put(encodedText, Map.entry(encodedText, LocalDateTime.now().plusSeconds(5)));
+        }
 
-    return ResponseEntity.ok(ownServerUrl+encodedText);
-  }
-
-  private String generateUniquePattern() {
-    String encodedText = UUID.randomUUID().toString().substring(0, 6);
-
-    while (map.get(encodedText) != null) {
-      encodedText = UUID.randomUUID().toString().substring(0, 6);
+        MessageFormat messageFormat = new MessageFormat("Map has been filled [size={0}]");
+        String text = messageFormat.format(new Object[]{map.size()});
+        return ResponseEntity.ok(text);
     }
 
-    return encodedText;
-  }
+    @GetMapping("/generate")
+    public ResponseEntity<String> generate(@RequestParam String text) {
+        String encodedText = generateUniquePattern();
 
-  @GetMapping("/{url}")
-  public String getUrl(
-      @PathVariable(value = "url") String url, HttpServletResponse httpServletResponse) {
+        map.put(encodedText, Map.entry(text, LocalDateTime.now().plusSeconds(60)));
 
-    var pair = map.get(url);
-    if (pair == null) {
-      return "NO SHORTCODE FOR THIS";
+        String baseUrl = ownServerUrl + applicationContext.getWebServer().getPort()+"/";
+        return ResponseEntity.ok(baseUrl + encodedText);
     }
 
-    // retrieve the decodedUrl and update the map to extend the url use
-    String decodedUrl = pair.getKey();
-    LocalDateTime extendedDuration = LocalDateTime.now().plusMinutes(1);
-    map.put(url, Map.entry(decodedUrl, extendedDuration));
-    logger.info("extending the url [key={};value={};until={}]", url, decodedUrl, extendedDuration);
+    private String generateUniquePattern() {
+        String encodedText = UUID.randomUUID().toString().substring(0, 6);
 
-    // redirect the user
-    httpServletResponse.setHeader("Location", decodedUrl);
-    httpServletResponse.setStatus(302);
+        while (map.get(encodedText) != null) {
+            encodedText = UUID.randomUUID().toString().substring(0, 6);
+        }
 
-    return decodedUrl;
-  }
+        return encodedText;
+    }
+
+    @GetMapping("/{url}")
+    public String getUrl(
+            @PathVariable(value = "url") String url, HttpServletResponse httpServletResponse) {
+
+        var pair = map.get(url);
+        if (pair == null) {
+            return "NO SHORTCODE FOR THIS";
+        }
+
+        // retrieve the decodedUrl and update the map to extend the url use
+        String decodedUrl = pair.getKey();
+        LocalDateTime extendedDuration = LocalDateTime.now().plusMinutes(1);
+        map.put(url, Map.entry(decodedUrl, extendedDuration));
+        logger.info("extending the url [key={};value={};until={}]", url, decodedUrl, extendedDuration);
+
+        // redirect the user
+        httpServletResponse.setHeader("Location", decodedUrl);
+        httpServletResponse.setStatus(302);
+
+        return decodedUrl;
+    }
 }
