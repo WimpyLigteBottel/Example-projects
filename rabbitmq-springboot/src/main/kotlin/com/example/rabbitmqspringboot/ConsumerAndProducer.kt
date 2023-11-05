2ca254e19a7c5c146a292e0e1095929ec44dd7af
mnel <mnel@bol.com>
2023-11-05 13:19:02 +0100

    Simplifying all the beans down to annotations and spring handles the bean setup and autowiring

--- rabbitmq-springboot/src/main/kotlin/com/example/rabbitmqspringboot/ConsumerAndProducer.kt
+++ rabbitmq-springboot/src/main/kotlin/com/example/rabbitmqspringboot/ConsumerAndProducer.kt
@@ -2,30 +2,46 @@ package com.example.rabbitmqspringboot
 
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.core.Queue
+import org.springframework.amqp.rabbit.annotation.RabbitHandler
+import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 
 
+@Configuration
+class Config {
+    @Bean
+    fun queuePrintln(): Queue {
+        return Queue("println", false)
+    }
+
+    @Bean
+    fun queueLog(): Queue {
+        return Queue("logInfo", false)
+    }
+}
 @Component
+@RabbitListener(queues = ["logInfo"])
 class Consumer {
 
     val log = LoggerFactory.getLogger(this::class.java)
-
-    //This default method to use if not specifying in the message ListenerAdapter
-    fun handleMessage(message: String) {
-        println("RECEIVED MESSAGE: $message")
-    }
-
-    fun printOutMessage(message: String) {
-        println("RECEIVED MESSAGE: $message")
-    }
-
+    @RabbitHandler
     fun logMessage(message: String) {
         log.info("RECEIVED MESSAGE: $message")
     }
 }
 
+@RabbitListener(queues = ["println"])
+@Component
+class ConsumerPrintln {
+    @RabbitHandler(isDefault = true)
+    fun handleMessage(message: String) {
+        println("RECEIVED MESSAGE: $message")
+    }
+}
 
 @Component
 class Publisher {
