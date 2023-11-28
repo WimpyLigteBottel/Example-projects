844eb45379ebe37ffea765f97da8bc6b972b432d
mnel <mnel@bol.com>
2023-11-28 13:04:26 +0100

    Upping the version

--- JWTExample/src/main/java/nel/marco/person/PersonRestEndpoint.java
+++ JWTExample/src/main/java/nel/marco/person/PersonRestEndpoint.java
@@ -6,7 +6,6 @@ import nel.marco.auth.AccessType;
 import nel.marco.auth.AuthenticationRestEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +26,7 @@ public class PersonRestEndpoint {
 
     List<Person> people = new ArrayList<>();
 
-    @GetMapping(value = "/person", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/person")
     public ResponseEntity<?> getPerson(HttpServletRequest request) {
         DecodedJWT decodedJWT;
         try {
