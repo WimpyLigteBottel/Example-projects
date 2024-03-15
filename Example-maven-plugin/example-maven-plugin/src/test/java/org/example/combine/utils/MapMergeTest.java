ab30501ab206b09a23870f0946556d2e41c48054
Marco-PC\Marco <epossmurfmarco@gmail.com>
2024-03-16 00:34:20 +0100

    refactoring

--- Example-maven-plugin/example-maven-plugin/src/test/java/org/example/combine/utils/MapMergeTest.java
+++ Example-maven-plugin/example-maven-plugin/src/test/java/org/example/combine/utils/MapMergeTest.java
@@ -2,6 +2,7 @@ package org.example.combine.utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.Yaml;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,55 +11,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MapMergeTest {
 
+    Yaml YAML = new Yaml();
+
+
     @Test
-    @DisplayName("very basic merge between a and b")
-    void deepMerge() {
-        var a = new HashMap<String, Object>();
-        a.put("a", "A");
-        var b = new HashMap<String, Object>();
-        b.put("b", "B");
+    @DisplayName("very basic merge between a and b test")
+    void deepMerge_2() {
+
+        var mapA = YAML.loadAs("""
+                a: "value1"
+                """, HashMap.class);
+        var mapB = YAML.loadAs("""
+                b: "value2"
+                """, HashMap.class);
+
+        var actual = MapMerge.deepMerge(mapA, mapB);
 
 
-        var actual = MapMerge.deepMerge(a, b);
-
-
-        var expected = new HashMap<>();
-        expected.putAll(a);
-        expected.putAll(b);
-
-
-        assertEquals(expected, actual);
+        assertEquals("value1", actual.get("a"));
+        assertEquals("value2", actual.get("b"));
     }
 
 
     @Test
     @DisplayName("more complex merge between a and b")
     void moreComplexDeepMerge() {
-        var a = new HashMap<String, Object>();
-        a.put("0",
-                Map.of("1A",
-                        Map.of("2A",
-                                Map.of("3A", Map.of())
-                        )
-                )
-        );
 
-        var b = new HashMap<String, Object>();
-        b.put("0",
-                Map.of("1B",
-                        Map.of("2B", Map.of())
-                )
-        );
+        HashMap<String, Object> a = YAML.loadAs("""
+                a:
+                    1A:
+                        2A:
+                            3A: ""
+                """, HashMap.class);
+
+
+        HashMap<String, Object> b = YAML.loadAs("""
+                a:
+                    1B:
+                        2B: ""
+                """, HashMap.class);
 
 
         var actual = MapMerge.deepMerge(a, b);
 
 
         var expected = new HashMap<>();
-        expected.put("0",
+        expected.put("a",
                 Map.of(
-                        "1A", Map.of("2A", Map.of("3A", Map.of())),
-                        "1B", Map.of("2B", Map.of())
+                        "1A", Map.of("2A", Map.of("3A", "")),
+                        "1B", Map.of("2B", "")
                 )
         );
 
