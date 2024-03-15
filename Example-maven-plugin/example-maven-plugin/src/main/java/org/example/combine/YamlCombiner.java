fe43d4f185f8b4c70fd1fe0a72e3ebfb3e0673b8
Marco-PC\Marco <epossmurfmarco@gmail.com>
2024-03-16 00:17:05 +0100

    refactoring

--- Example-maven-plugin/example-maven-plugin/src/main/java/org/example/combine/YamlCombiner.java
+++ Example-maven-plugin/example-maven-plugin/src/main/java/org/example/combine/YamlCombiner.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class YamlCombiner {
@@ -32,9 +34,31 @@ public class YamlCombiner {
         return specs.stream()
                 .map(YamlUtil::loadFile)
                 .reduce(MapMerge::deepMerge) // merge files 1 by 1
-                .map(YamlUtil::sortToOpenApiStyle)
+                .map(this::sortToOpenApiStyle)
                 .map(YamlUtil::toYamlString)
                 .orElseThrow();
+    }
+
+
+    /**
+     * The map will be sorted in semi readable way for open api specs.
+     * <p></p>
+     * <p>
+     * Example:
+     * base properties
+     * paths
+     * components
+     */
+    public Map<String, Object> sortToOpenApiStyle(Map<String, Object> mapToBeSorted) {
+        //This
+        LinkedHashMap<String, Object> linkedHashMap = new LinkedHashMap<>(mapToBeSorted);
+
+        linkedHashMap.remove("paths");
+        linkedHashMap.put("paths", mapToBeSorted.get("paths"));
+
+        linkedHashMap.remove("components");
+        linkedHashMap.put("components", mapToBeSorted.get("components"));
+        return linkedHashMap;
     }
 
 }
--- Example-maven-plugin/example-maven-plugin/src/main/java/org/example/combine/utils/YamlUtil.java
+++ Example-maven-plugin/example-maven-plugin/src/main/java/org/example/combine/utils/YamlUtil.java
@@ -58,26 +58,7 @@ public class YamlUtil {
         }
     }
 
-    /**
-     * The map will be sorted in semi readable way for open api specs.
-     * <p></p>
-     * <p>
-     * Example:
-     * base properties
-     * paths
-     * components
-     */
-    public static Map<String, Object> sortToOpenApiStyle(Map<String, Object> mapToBeSorted) {
-        //This
-        LinkedHashMap<String, Object> linkedHashMap = new LinkedHashMap<>(mapToBeSorted);
 
-        linkedHashMap.remove("paths");
-        linkedHashMap.put("paths", mapToBeSorted.get("paths"));
-
-        linkedHashMap.remove("components");
-        linkedHashMap.put("components", mapToBeSorted.get("components"));
-        return linkedHashMap;
-    }
 
 
     public static <T> T loadFile(String path, Class<T> expectResponse) {
