39a1ca2a0a165577227e2c8d0d986c42ef7873fb
Marco-PC\Marco <epossmurfmarco@gmail.com>
2024-03-16 00:02:32 +0100

    refactoring locust testing

--- LoadtestGenerator/loadtesting/shaped_locustfile.py
+++ LoadtestGenerator/loadtesting/shaped_locustfile.py
@@ -4,21 +4,24 @@ from DeleteTasks import DeleteTasks
 from GetTasks import GetTasks
 from PostTasks import PostTasks
 from PutTasks import PutTasks
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-#Define all your tasks here in this class
+# Define all your tasks here in this class
 class WebsiteUser(HttpUser):
     wait_time = constant_throughput(1)  # wait time between requests, in seconds
     tasks = {
-        GetTasks: 10,   # Adjust the weight according to your needs
-        DeleteTasks: 1,   # Adjust the weight according to your needs
-        PostTasks: 2,   # Adjust the weight according to your needs
-        PutTasks: 3    # Adjust the weight according to your needs
+        GetTasks: 10,  # Adjust the weight according to your needs
+        DeleteTasks: 1,  # Adjust the weight according to your needs
+        PostTasks: 2,  # Adjust the weight according to your needs
+        PutTasks: 3  # Adjust the weight according to your needs
     }
 
 
-
-#Define your loadshape in here
+# Define your loadshape in here
 
 class StagesShape(LoadTestShape):
     """
@@ -40,30 +43,30 @@ class StagesShape(LoadTestShape):
     stages = [
         {"duration": 5, "users": 100, "spawn_rate": 20},
         {"duration": 10, "users": 200, "spawn_rate": 40},
-        # {"duration": 60, "users": 400, "spawn_rate": 50},
-        # {"duration": 80, "users": 350, "spawn_rate": 50},
-        # {"duration": 100, "users": 550, "spawn_rate": 50},
-        # {"duration": 120, "users": 300, "spawn_rate": 50},
-        # {"duration": 140, "users": 370, "spawn_rate": 50},
-        # {"duration": 160, "users": 10, "spawn_rate": 50},
-        # {"duration": 200, "users": 500, "spawn_rate": 50},
-        # {"duration": 220, "users": 500, "spawn_rate": 50},
+        {"duration": 60, "users": 400, "spawn_rate": 50},
+        {"duration": 80, "users": 350, "spawn_rate": 50},
+        {"duration": 100, "users": 550, "spawn_rate": 50},
+        {"duration": 120, "users": 300, "spawn_rate": 50},
+        {"duration": 140, "users": 370, "spawn_rate": 50},
+        {"duration": 160, "users": 10, "spawn_rate": 50},
+        {"duration": 200, "users": 500, "spawn_rate": 50},
+        {"duration": 220, "users": 500, "spawn_rate": 50},
     ]
 
     def tick(self):
         run_time = self.get_run_time()
-
-        # Calculate the total duration of all stages
-        total_duration = sum(stage["duration"] for stage in self.stages)
 
         for stage in self.stages:
             if run_time < stage["duration"]:
                 tick_data = (stage["users"], stage["spawn_rate"])
                 return tick_data
 
-                # Reset run_time when all stages have completed
-        if run_time >= total_duration:
-            self.start_time = 0
-            self.tick()
+        self.reset_time()
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                tick_data = (stage["users"], stage["spawn_rate"])
+                return tick_data
 
         return None
