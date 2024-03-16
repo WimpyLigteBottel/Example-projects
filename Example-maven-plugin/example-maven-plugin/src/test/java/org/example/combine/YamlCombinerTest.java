53ad5c5b7e294e13fed8e845c7e4d2cdcca033b9
Marco-PC\Marco <epossmurfmarco@gmail.com>
2024-03-16 01:43:55 +0100

    refactoring

--- Example-maven-plugin/example-maven-plugin/src/test/java/org/example/combine/YamlCombinerTest.java
+++ Example-maven-plugin/example-maven-plugin/src/test/java/org/example/combine/YamlCombinerTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class YamlCombinerTest {
 
     String common = "src/test/resources/common.yaml";
     String commonSecond = "src/test/resources/common-second.yaml";
+    String commonThird = "src/test/resources/shared/common-third.yaml";
     String usingCommon = "src/test/resources/using-common.yaml";
 
     @Test
@@ -58,5 +58,47 @@ class YamlCombinerTest {
 
         //these should not exist anymore
         assertFalse(actual.contains("$ref: \"./common.yaml#/components/schemas/Error\""));
+    }
+
+
+    /**
+     * Currently there is bug regarding this. the ref cleanup is not working correctly on sub folders
+     */
+    @Test
+    @DisplayName("./shared/common-third.yaml is removed in $ref")
+    void expect_fullExternalFileRefToBeUpdated() {
+        var yamlCombiner = new YamlCombiner();
+
+        //Checks that the references exist in the file
+        var before = yamlCombiner.getCombinedYamlOuputAsString(Collections.singletonList(usingCommon));
+
+        assertTrue(before.contains("$ref: \"./shared/common-third.yaml#/components/schemas/ErrorThird\""));
+
+
+        var actual = yamlCombiner.getCombinedYamlOuputAsString(List.of(commonThird, usingCommon));
+
+        var expected = """
+                ---
+                components:
+                  schemas:
+                    ErrorThird:
+                      type: "object"
+                      properties:
+                        id:
+                          type: "integer"
+                        name:
+                          type: "string"
+                      xml:
+                        name: "category"
+                    Custom3:
+                      $ref: "#/components/schemas/ErrorThird"
+                    Custom:
+                      $ref: "./common.yaml#/components/schemas/Error"
+                    Custom2:
+                      $ref: "./common-second.yaml#/components/schemas/ErrorSecond"
+                """;
+
+
+        assertEquals(expected, actual);
     }
 }
