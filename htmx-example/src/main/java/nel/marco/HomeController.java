cc2da31605a41a5c0edd9d99cc870b408115958e
Marco-PC\Marco <epossmurfmarco@gmail.com>
2024-03-17 22:43:36 +0100

    Adding post call

--- htmx-example/src/main/java/nel/marco/HomeController.java
+++ htmx-example/src/main/java/nel/marco/HomeController.java
@@ -3,8 +3,12 @@ package nel.marco;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class HomeController {
@@ -21,10 +25,19 @@ public class HomeController {
 class CustomRestController {
 
     @GetMapping("/hello")
-    public String furtherInfo(@RequestParam(required = false) String name,
-                              @RequestParam(required = false) String surname) {
+    public String furtherInfo(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String surname
+    ) {
 
         return name + " " + surname;
+    }
+
+    @PostMapping("/color")
+    public String changeColor() {
+        var colors = List.of("red", "blue", "green");
+
+        return colors.get(ThreadLocalRandom.current().nextInt(0, colors.size() - 1));
     }
 
 }
