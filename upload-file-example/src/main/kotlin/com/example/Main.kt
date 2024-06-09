2d9bf0e78785918640cafea82f41bec9471c8ec7
mnel <epos.smurfmarco@gmail.com>
2024-06-09 19:48:05 +0200

    Updating how files are being uploaded

--- upload-file-example/src/main/kotlin/com/example/Main.kt
+++ upload-file-example/src/main/kotlin/com/example/Main.kt
@@ -12,8 +12,9 @@ import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
-import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
@@ -34,26 +35,36 @@ open class Endpoint {
 
     @PostMapping("/upload")
     fun handleFile(
-        @RequestPart("file1", required = false) file: Resource? = null,
-        @RequestPart("file2", required = false) file2: Resource? = null
+        @RequestPart("file1", required = false) file: MultipartFile? = null,
+        @RequestPart("file2", required = false) file2: MultipartFile? = null
     ): String {
 
         file?.let {
-            log.info("fileName={}", it.filename)
-            log.info("file contents={}", it.getContentAsString(Charset.defaultCharset()))
-            createCopy(it, "1")
+            log.info("fileName={}", it.originalFilename)
+            createCopy(it.resource, "1")
         }
 
         file2?.let {
-            log.info("fileName={}", it.filename)
-            log.info("file contents={}", it.getContentAsString(Charset.defaultCharset()))
-            createCopy(it, "2")
+            log.info("fileName={}", it.originalFilename)
+            createCopy(it.resource, "2")
         }
 
         if (file == null && file2 == null)
             return "No file"
         else
-            return "File uploaded [file1=${file?.filename};file2=${file2?.filename}]"
+            return "File uploaded [file1=${file?.originalFilename};file2=${file2?.originalFilename}]"
+    }
+
+    @PostMapping("/upload-multiples")
+    fun handleFiles(
+        @RequestPart("files", required = false) files: List<MultipartFile> = emptyList(),
+    ): String {
+        files.forEachIndexed { index, resource ->
+            log.info("fileName={}", resource.originalFilename)
+            createCopy(resource.resource, "$index")
+        }
+
+        return "Files uploaded [count=${files.size}]"
     }
 
     private fun createCopy(file: Resource, version: String) {
@@ -73,32 +84,60 @@ open class UploadOnRunTime : CommandLineRunner {
     private val log = LoggerFactory.getLogger(this::class.java)
 
     override fun run(vararg args: String?) {
-        val builder = MultipartBodyBuilder()
-
         kotlin.runCatching {
-            val file = ClassPathResource("picture.jpg").file
+            singleUpload()
+            mutlipleUpload()
+        }
+    }
+
+    private fun mutlipleUpload() {
+        val builder = MultipartBodyBuilder()
+        val file = ClassPathResource("picture.jpg").file
+
+        for (x in 1..10) {
             builder
-                .part("file1", file.readBytes())
+                .part("files", file.readBytes())
                 .header(
                     "Content-Disposition",
-                    ContentDisposition.formData().name("file1").filename(file.name).build().toString()
+                    ContentDisposition.formData().name("files").filename(x.toString()).build().toString()
                 )
-
-            builder
-                .part("file2", file.readBytes())
-                .header(
-                    "Content-Disposition",
-                    ContentDisposition.formData().name("file2").filename(file.name).build().toString()
-                )
-
-            val response = WebClient.create().post().uri("http://localhost:8080/upload")
-                .bodyValue(builder.build())
-                .retrieve()
-                .toEntity(String::class.java)
-                .block()
-
-             log.info("${response?.statusCode};${response?.body}")
         }
 
+        val response = WebClient.create()
+            .post()
+            .uri("http://localhost:8080/upload-multiples")
+            .body(BodyInserters.fromMultipartData(builder.build()))
+            .retrieve()
+            .toEntity(String::class.java)
+            .block()
+
+        log.info("${response?.statusCode};${response?.body}")
+    }
+
+    private fun singleUpload() {
+        val builder = MultipartBodyBuilder()
+
+        val file = ClassPathResource("picture.jpg").file
+        builder
+            .part("file1", file.readBytes())
+            .header(
+                "Content-Disposition",
+                ContentDisposition.formData().name("file1").filename(file.name).build().toString()
+            )
+
+        builder
+            .part("file2", file.readBytes())
+            .header(
+                "Content-Disposition",
+                ContentDisposition.formData().name("file2").filename(file.name).build().toString()
+            )
+
+        val response = WebClient.create().post().uri("http://localhost:8080/upload")
+            .bodyValue(builder.build())
+            .retrieve()
+            .toEntity(String::class.java)
+            .block()
+
+        log.info("${response?.statusCode};${response?.body}")
     }
 }
