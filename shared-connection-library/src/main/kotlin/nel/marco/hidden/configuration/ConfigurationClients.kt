0e0b31c6d3232d96eefd58229646eba734bd1990
mnel <epos.smurfmarco@gmail.com>
2024-07-21 20:59:21 +0200

    Updating shared library config

--- shared-connection-library/src/main/kotlin/nel/marco/hidden/configuration/ConfigurationClients.kt
+++ shared-connection-library/src/main/kotlin/nel/marco/hidden/configuration/ConfigurationClients.kt
@@ -5,17 +5,53 @@ import nel.marco.hidden.clients.OrderBasicHttpClient
 import nel.marco.hidden.clients.OrderDeliveryHttpClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-class ConfigurationClients {
+class ConfigurationClient(
+    configurationClientsProperties: ConfigurationClientsProperties,
+    environment: Environment
+) {
+    private val config: ConfigProperties = when {
+        environment.matchesProfiles("pro") -> configurationClientsProperties.pro
+        environment.matchesProfiles("stg") -> configurationClientsProperties.stg
+        else -> configurationClientsProperties.stg
+    }
 
     @Bean
-    fun orderBasicHttpClient(webClient: WebClient) = OrderBasicHttpClient(webClient)
+    fun orderBasicHttpClient(webClient: WebClient.Builder) =
+        OrderBasicHttpClient(webClient.baseUrl(config.orderUrl).build())
 
     @Bean
-    fun orderDeliveryHttpClient(webClient: WebClient) = OrderDeliveryHttpClient(webClient)
+    fun orderDeliveryHttpClient(webClient: WebClient.Builder) =
+        OrderDeliveryHttpClient(webClient.baseUrl(config.deliveryUrl).build())
 
     @Bean
-    fun customerHttpClient(webClient: WebClient) = CustomerHttpClient(webClient)
+    fun customerHttpClient(webClient: WebClient.Builder) =
+        CustomerHttpClient(webClient.baseUrl(config.customerUrl).build())
+}
+
+@Configuration
+class ConfigurationClientsProperties {
+    lateinit var stg: ConfigProperties.ConfigurationStg
+    lateinit var pro: ConfigProperties.ConfigurationPro
+}
+
+sealed class ConfigProperties(
+    val orderUrl: String,
+    val deliveryUrl: String,
+    val customerUrl: String
+) {
+    class ConfigurationStg : ConfigProperties(
+        orderUrl = "http://stg.url:8080",
+        deliveryUrl = "http://stg.url:8081",
+        customerUrl = "http://stg.url:8082",
+    )
+
+    class ConfigurationPro : ConfigProperties(
+        orderUrl = "http://pro.url:8080",
+        deliveryUrl = "http://pro.url:8081",
+        customerUrl = "http://pro.url:8082",
+    )
 }
