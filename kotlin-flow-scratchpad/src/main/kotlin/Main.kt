d5d5d0eb78a78c367230164ac0d551cc80a9c25a
mnel <epos.smurfmarco@gmail.com>
2024-08-13 23:04:24 +0200

    Trying to make shared state example

--- kotlin-flow-scratchpad/src/main/kotlin/Main.kt
+++ kotlin-flow-scratchpad/src/main/kotlin/Main.kt
@@ -1,38 +1,57 @@
 package nel.marco
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.toList
-import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.flow.onEach
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-suspend fun main() {
-
-    val time = measureTimeMillis {
-
-        val fast = generateNumbers(10)
-        val slow = generateNumbers(10)
-
-        withContext(Dispatchers.IO) {
-            val a = async(Dispatchers.IO) { fast.toList() }
-            val b = async(Dispatchers.IO) { slow.toList() }
-
-            awaitAll(a, b)
-
-            println(a.await())
-            println(b.await())
-
-        }
-    }
-    println("$time ms")
+fun main() {
+    runCatching { runBlocking { sharedStateFlowExample() } }
 }
 
 
-suspend fun generateNumbers(amountOfNumber: Int) = withContext(Dispatchers.IO) {
+/**
+ * Showcases how single event can be consumed by multiple other flows and perform an action
+ */
+suspend fun sharedStateFlowExample() {
+    val personA = MutableSharedFlow<String>()
+    val personB = MutableSharedFlow<String>()
+
+    withContext(Dispatchers.IO) {
+        launch {
+            personA.collect {
+                println(it)
+            }
+        }
+        launch {
+            personB.collect {
+                println(it)
+            }
+        }
+
+        generateNumbers(10, 100)
+            .collect {
+                personA.emit("Person A = $it")
+                personB.emit("Person B = $it")
+            }
+
+        cancel("Stop processing because flows go on forever!")
+    }
+
+}
+
+
+/**
+ * Generates numbers until the amount specified has been generated with delay in between based on the delayAmount in milliseconds
+ */
+suspend fun generateNumbers(amountOfNumber: Int, delayAmount: Long = 1000) = withContext(Dispatchers.IO) {
     flow {
         repeat(amountOfNumber) {
-            delay(1000)
+            delay(delayAmount)
             emit(it)
         }
     }
