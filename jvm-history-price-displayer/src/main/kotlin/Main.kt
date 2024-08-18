0b2a229a6e2706b3499807c33ec1746e0426e15e
mnel <epos.smurfmarco@gmail.com>
2024-08-18 17:29:17 +0200

    example project to display xy chart

--- jvm-history-price-displayer/src/main/kotlin/Main.kt
+++ jvm-history-price-displayer/src/main/kotlin/Main.kt
@@ -1,16 +1,55 @@
 package nel.marco
 
+import org.knowm.xchart.SwingWrapper
+import org.knowm.xchart.XYChart
+import org.knowm.xchart.XYChartBuilder
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.*
+import kotlin.random.Random
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val chart: XYChart = XYChartBuilder().width(800).height(600)
+        .title("Price Over Time")
+        .xAxisTitle("Date")
+        .yAxisTitle("Price")
+        .build()
+
+    chart.styler.datePattern = "yyyy-MM-dd"
+    chart.styler.isXAxisTicksVisible = true // Ensure x-axis tick marks are visible
+    chart.styler.xAxisLabelRotation = 65 // Rotate labels for better visibility (optional)
+
+    val apples = generateData("Apple", 14)
+    val banana = generateData("Banana", 7)
+    val candy = generateData("Candy", 21)
+
+    chart.addSeries(apples.first().name, apples.map { it.localDate }, apples.map { it.price })
+    chart.addSeries(banana.first().name, banana.map { it.localDate }, banana.map { it.price })
+    chart.addSeries(candy.first().name, candy.map { it.localDate }, candy.map { it.price })
+
+    // Show it
+    SwingWrapper(chart).displayChart();
+
+}
+
+fun LocalDate.toDate(): Date {
+    return Date.from(this.atStartOfDay(ZoneId.systemDefault()).toInstant())
+}
+
+data class History(val id: String, val name: String, val price: Double, val localDate: Date)
+
+fun generateData(name: String, pastDays: Long = 14): List<History> {
+    return (1..pastDays)
+        .map {
+            History(
+                id = UUID.randomUUID().toString(),
+                name = name,
+                price = Random.nextDouble(20.0, 80.0),
+                localDate = LocalDate.now().minusDays(it).toDate()
+            )
+        }.sortedBy { it.localDate }
 }
