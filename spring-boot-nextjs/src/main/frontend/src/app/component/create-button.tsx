532945e1b3f7084a6e34d6ebeb4b20b3ecbef00d
mnel <epos.smurfmarco@gmail.com>
2024-09-02 00:29:26 +0200

    Adding loader on create button

--- spring-boot-nextjs/src/main/frontend/src/app/component/create-button.tsx
+++ spring-boot-nextjs/src/main/frontend/src/app/component/create-button.tsx
@@ -1,15 +1,24 @@
 "use client";
 import { createRandomPerson } from "@/integration/POST-person-fetcher";
+import { useState } from "react";
+import Spinner from "@/component/spinner/spinner";
 
 const CreateButton = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <button
       className="clickMeBtn"
+      disabled={isLoading}
       onClick={() => {
+        setIsLoading(true);
         createRandomPerson();
+        setTimeout(()=>{
+            setIsLoading(false);
+        }, 100);
       }}
     >
-      Click me!
+       {!isLoading ? "Create random User!" : <Spinner />}
     </button>
   );
 };
