0c9fc8490d191ac38961d69821966fa6e971eb06
mnel <mnel@bol.com>
2024-10-21 18:38:47 +0200

    adding comment

--- coroutine-gotcha-story/src/main/kotlin/nel/marco/DeadlockExample.kt
+++ coroutine-gotcha-story/src/main/kotlin/nel/marco/DeadlockExample.kt
@@ -11,6 +11,24 @@ val workers = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
 val countDownLatch = CountDownLatch(1)
 
 
+/*
+What is going to print?
+
+1.
+Worker A: Tell me when i can start!
+Worker B: YOU CAN START!
+Worker B: DONE!
+Worker A: DONE!
+
+
+2.
+
+Launching tasks!
+Worker A: Tell me when i can start!
+
+ */
+
+
 fun main(): Unit {
     runBlocking(workers) {
 
