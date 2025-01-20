4059336bc4db93176565603acd94f872e00bbdc4
mnel <epos.smurfmarco@gmail.com>
2025-01-20 23:20:03 +0100

    Adding current webfilters... Still need to fix working example of aspect

---
+++ web-filters/src/main/kotlin/nel/marco/Launcher.kt
@@ -0,0 +1,48 @@
+package nel.marco
+
+import kotlinx.coroutines.reactor.ReactorContext
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import kotlin.coroutines.coroutineContext
+
+@SpringBootApplication
+open class Launcher
+
+
+fun main(args: Array<String>) {
+    runApplication<Launcher>(*args)
+}
+
+
+@Component
+class OnStartUp : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        WebClient.create("http://localhost:8080").get().uri("/hello-world").retrieve().toBodilessEntity().block()
+
+        System.exit(0)
+    }
+
+}
+
+
+@RestController
+class Users {
+
+    @GetMapping("/hello-world")
+    suspend fun getUsers(): String {
+        someCoroutineHandler()
+        return "Users"
+    }
+
+
+    suspend fun someCoroutineHandler() {
+        val customContext = coroutineContext[ReactorContext]?.context?.get<CustomContext>(CustomContext)
+        println("1st. Accessed CUSTOM_ID in Coroutine: ${customContext?.getCustomId()}")
+    }
+
+}
