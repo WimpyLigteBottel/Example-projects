6cf6a279d13736bd202d9f1728ad082da156cfea
mnel <epos.smurfmarco@gmail.com>
2025-06-09 13:45:46 +0200

    adding kotlin collection example

--- kotlin/kotlin-collection-examples/src/main/kotlin/Main.kt
+++ kotlin/kotlin-collection-examples/src/main/kotlin/Main.kt
@@ -5,16 +5,33 @@ fun main() {
     val list = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
     associateExample(list)
     chunkExample(list)
+    zipExample(list, list)
+    partitionExample(list)
 }
 
 
 fun associateExample(list: List<String>) {
-    val map: Map<String, String> = list.associate { value -> value to value }
-    println(map)
+    println("associate: " + list.associate { value -> value to value })
+    var charL = 'a'.toInt() - 1
+    println("associateBy: " + list.associateBy {
+        charL++
+        charL.toChar()
+    })
 }
 
 
 fun chunkExample(list: List<String>) {
-    val listOfSmallList = list.chunked(3)
-    println(listOfSmallList)
+    println("Chunked: ${list.chunked(3)}")
+}
+
+
+fun zipExample(list: List<String>, listB: List<String>) {
+    println("zip: ${list.zip(listB)}")
+}
+
+
+fun partitionExample(list: List<String>) {
+    val listOfSmallList = list.partition { it.toLong() > 5 }
+    println("(partition) greater than 5 : ${listOfSmallList.first}")
+    println("(partition) smaller than 5 : ${listOfSmallList.second}")
 }
