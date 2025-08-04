922681b96bb42b79116bee876c19b39e1ab80501
mnel <epos.smurfmarco@gmail.com>
2025-08-04 21:11:50 +0200

    refactoring

--- gotcha/kotlin-compile/src/main/kotlin/nel/marco/Launcher.kt
+++ gotcha/kotlin-compile/src/main/kotlin/nel/marco/Launcher.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @SpringBootApplication
 class Launcher
 
@@ -13,10 +12,8 @@ fun main(args: Array<String>) {
     runApplication<Launcher>(*args)
 }
 
-
 @RestController
-class Endpoint {
-
+open class Endpoint {
     @GetMapping("/helloworld")
     fun main(): String = "Hello world"
 }
