3e7ab35e819d16658b42f96cd33cc787777a4b12
mnel <epos.smurfmarco@gmail.com>
2025-08-27 21:56:19 +0200

    Adding maven gotcha

---
+++ gotcha/maven-dependency-gotcha/springbootapp/src/main/kotlin/nel/marco/Main.kt
@@ -0,0 +1,26 @@
+package nel.marco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringApplication>(*args)
+}
+
+
+/*
+
+Question:
+
+1. Is my application reactive  / servlet based?
+
+
+2. Why is it A and when will it be B?
+
+
+
+
+ */
