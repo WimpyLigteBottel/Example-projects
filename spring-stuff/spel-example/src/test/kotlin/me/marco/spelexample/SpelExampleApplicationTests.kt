f02c8b6c1e9dbef70c2fd3fff090c9c75085611c
mnel <epos.smurfmarco@gmail.com>
2026-04-14 15:34:49 +0200

    Ading spel example

---
+++ spring-stuff/spel-example/src/test/kotlin/me/marco/spelexample/SpelExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.marco.spelexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpelExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
