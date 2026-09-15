ce506ff068ced483ff346457bc68621a36994a0f
mnel <epos.smurfmarco@gmail.com>
2026-09-15 20:13:23 +0200

    updating redis code

--- spring-stuff/redis-springboot/src/main/kotlin/com/example/redis/ApplicationLauncher.kt
+++ spring-stuff/redis-springboot/src/main/kotlin/com/example/redis/ApplicationLauncher.kt
@@ -1,4 +1,4 @@
-package com.example.rabbitmqspringboot
+package com.example.redis
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
--- spring-stuff/redis-springboot/src/test/kotlin/com/example/redis/ApplicationLauncherTests.kt
+++ spring-stuff/redis-springboot/src/test/kotlin/com/example/redis/ApplicationLauncherTests.kt
@@ -1,4 +1,4 @@
-package com.example.rabbitmqspringboot
+package com.example.redis
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
